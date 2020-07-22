c198ef505c76024cb84e5b4ddc5f7ab783eb7b00
Bradley Petty-Fitzmaurice <bradpfm@gmail.com>
2020-07-22 22:29:14 +0100

    more fixing

--- bannertext.js
+++ bannertext.js
@@ -1,4 +1,4 @@
-import Typed from 'https://bradpfm.github.io/node_modules/typed.js/src/typed.js';
+import Typed from 'node_modules/typed.js/src/typed.js';
  console.log("hello")
  const loadDynamicBannerText = () => {
    console.log("hello");
